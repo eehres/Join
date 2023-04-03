ef46bd76c0c1d20e238d85512dba112af93fb576
ahn <201717504@kyonggi.ac.kr>
2023-04-03 20:39:08 +0900

    start

---
+++ server/src/test/java/com/join/joinUs/JoinUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.join.joinUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
